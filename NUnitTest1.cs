0cc1862f2280b833514aa27cb48b095bb8c05057
hanarayanan <30805136+hanarayanan@users.noreply.github.com>
2017-08-07 23:04:58 +0530

    commit test

---
+++ NUnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//using NUnit.Framework;
+using WindowsServiceBAL;
+using NUnit.Framework;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+      
+
+
+
+        [Test]
+        public void TestMethodNunit1()
+        {
+            
+            int result = 20 + 1000;
+            Assert.AreEqual(1020, result);
+        }
+    }
+
+    [TestFixture]
+    public class NUnitTest2
+    {
+
+
+        [Test]
+        public void HelloCheck()
+        {
+
+            int result = 20 + 10900;
+            Assert.AreEqual(10920, result);
+        }
+    }
+}
